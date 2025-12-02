25e8d7a0d56a26e18e50d5b2cce07ca847988710
SamyAguilar <samiaguilar321@gmail.com>
2025-12-01 23:50:02 -0600

    sam-ton-lulu
    3listo

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,12 +1,12 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'config/theme.dart';
 import 'providers/auth_provider.dart';
 import 'providers/theme_provider.dart';
-import 'providers/gemini_provider.dart';
+import 'providers/gemini_provider.dart'; // Mantener el provider de Lulu
 import 'services/storage_service.dart';
+import 'services/http_service.dart'; // Necesario para los nuevos providers
 import 'screens/splash_screen.dart';
 
 // 🆕 IMPORTA TUS PROVIDERS DE TOÑO
@@ -28,11 +28,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Se agregan los providers de Toño, inicializándolos con HttpService()
     return MultiProvider(
       providers: [
+        // Providers de Base
         ChangeNotifierProvider(create: (_) => AuthProvider()..init()),
         ChangeNotifierProvider(create: (_) => ThemeProvider()..init()),
-        ChangeNotifierProvider(create: (_) => GeminiProvider()), // <-- LULU Provider
+        ChangeNotifierProvider(create: (_) => GeminiProvider()), // Provider de Lulu
+
+        // 🆕 Providers del módulo de Materias (Toño)
+        ChangeNotifierProvider(
+          create: (_) => MateriaProvider(HttpService()),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ContenidoProvider(HttpService()),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ProgresoProvider(HttpService()),
+        ),
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, _) {
